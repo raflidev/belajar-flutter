2e08c5b28e8581cb55dac1b41563d580fd0443de
Rafli Ramadhan <rafliramdhn@gmail.com>
2022-05-17 17:37:22 +0700

    add sign up page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:airplane/ui/pages/get_started_page.dart';
+import 'package:airplane/ui/pages/sign_up_page.dart';
 import 'package:airplane/ui/pages/splash_page.dart';
 import 'package:flutter/material.dart';
 
@@ -8,9 +10,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SplashPage(),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, routes: {
+      '/': (context) => SplashPage(),
+      '/get-started': (context) => GetStartedPage(),
+      '/sign-up': (context) => SignUpPage(),
+    });
   }
 }
